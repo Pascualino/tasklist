2d91d6dd6523a07eeb1a905cc1d900e66a61ab11
Pina Canelles <canelles@ac87a300a726.ant.amazon.com>
2015-12-31 15:59:00 +0100

    Initial (WIP)

--- js/app.js
+++ js/app.js
@@ -1,199 +1,231 @@
 angular.module('tasklist', ['ionic'])
 
-
+    /**
+     * Stores and retrieves a JSON object from the server
+     */
     .value('serverEndpoint', 'https://7ycx0a0fkd.execute-api.us-west-2.amazonaws.com/prod/projectlist')
-
-    .factory('ProjectsService', ['serverEndpoint', function (serverEndpoint, $http) {
+    .factory('PersistenceService', function (serverEndpoint, $http) {
         return {
             loadFromServer: function () {
                 return $http.get(serverEndpoint);
             },
-            updateServerData: function (projects) {
+            updateServerData: function (content) {
+                console.log("Sending content: " + content);
                 return $http.post(
                     serverEndpoint,
                     {
-                        content: angular.toJson(projects)
+                        content: content
                     }
                 );
             }
         };
+    })
+
+    /**
+     * Creates Task objects
+     */
+    .factory('TaskFactory', function(){
+        return {
+            newTask: function(task){
+                return {
+                    title: task.title,
+                    importance: task.importance,
+                    time: task.time,
+                    active: task.active
+                };
+            },
+            newEmptyTask: function(task){
+                task.active = false;
+                return this.newTask(task);
+            },
+            taskFromJson: function(jsonTask){
+                return this.newTask(jsonTask);
+            }
+        }
+    })
+
+    /**
+     * Creates Project objects
+     */
+    .factory('ProjectFactory', ['TaskFactory', function(TaskFactory){
+        return {
+            newProject: function(projectTitle, tasks){
+                return {
+                    title: projectTitle,
+                    tasks: tasks,
+                    addNewTask: function(task){
+                        this.tasks.push(TaskFactory.newEmptyTask(task));
+                    },
+                    removeTask: function(task){
+                        var index = this.tasks.indexOf(task);
+                        this.tasks.splice(index, 1);
+                    }
+                };
+            },
+            newEmptyProject: function(projectTitle) {
+                return this.newProject(projectTitle, []);
+            },
+            projectFromJson: function(project){
+                var tasks = [];
+                project.tasks.forEach(function(element){
+                    tasks.push(TaskFactory.taskFromJson(element));
+                });
+
+                return this.newProject(project.title, tasks);
+            }
+        }
     }])
 
-    .factory('Projects', ['ProjectsServer', function(projectsServer){
+    /**
+     * Keeps and synchronizes the list of projects with their tasks, as well as the currently active project
+     */
+    .factory('ProjectList', function(PersistenceService, ProjectFactory, $ionicLoading){
         var projects = [];
-        var activeProject = 0;
+        var activeProject = {};
 
-        var projectsFactory;
-        projectsFactory = {
-            getProjects: function () {
+        var projectList = {
+            getProjects: function(){
                 return projects;
             },
-            refreshProjects: function () {
-                projectsServer.loadFromServer.success(
+            getActiveProject: function(){
+                return activeProject;
+            },
+            setActiveProject: function(project){
+                activeProject = project;
+            },
+            setProjectsFromJson: function(jsonProjects){
+                projects = [];
+                jsonProjects.forEach(function(e){
+                    projects.push(ProjectFactory.projectFromJson(e));
+                });
+            },
+            refreshProjects: function (additionalActionsOnSuccess) {
+                PersistenceService.loadFromServer().success(
                     function (value) {
-                        console.log("**" + value);
-                        projects = angular.fromJson(value);
+                        projectList.setProjectsFromJson(angular.fromJson(value));
                         activeProject = projects[0];
-                        console.log(projects);
-                        //additionalSuccess();
+                        console.log(value);
+                        if(additionalActionsOnSuccess){
+                            additionalActionsOnSuccess();
+                        }
                     }).error(function (error) {
                     alert(error);
                 })
             },
             updateProjects: function () {
-                projectsServer.updateServerData(projects);
+                PersistenceService.updateServerData(angular.toJson(projects))
+                    .success(function(){
+                        $ionicLoading.show({ template: 'Content synchronized!', noBackdrop: true, duration: 2000 });
+                    });
             },
             addNewProject: function (projectTitle) {
-                var newProject = {
-                    title: projectTitle,
-                    tasks: []
-                }
+                var newProject = ProjectFactory.newEmptyProject(projectTitle);
                 projects.push(newProject);
-                projectsFactory.updateProjects();
+                activeProject = newProject;
+                projectList.updateProjects();
+            },
+            removeTask: function(task){
+                activeProject.removeTask(task);
+                projectList.updateProjects();
+            },
+            removeProject: function(project){
+                var index = projects.indexOf(project);
+                projects.splice(index, 1);
+                console.log(angular.toJson(projects));
+                projectList.updateProjects();
             }
         };
-        projectsFactory.refreshProjects();
-        return projectsFactory;
-    }])
-
-    /**
-     * The Projects factory handles saving and loading projects
-     * from local storage, and also lets us save and load the
-     * last active project index.
-     */
-    .factory('Projects', function ($q, $http) {
-        return {
-            loadFromServer: function () {
-                return $http.get('https://7ycx0a0fkd.execute-api.us-west-2.amazonaws.com/prod/projectlist');
-            },
-            updateServerData: function (projects) {
-                console.log({
-                    content: angular.toJson(projects)
-                });
-                $http.post(
-                    'https://7ycx0a0fkd.execute-api.us-west-2.amazonaws.com/prod/projectlist',
-                    {content: angular.toJson(projects)}
-                ).then(function (value) {
-                    console.log("Saved into server");
-                }, function (error) {
-                    alert("Error saving into server: " + error);
-                    console.log(error);
-                });
-            },
-            newProject: function (projectTitle) {
-                // Add a new project
-                return {
-                    title: projectTitle,
-                    tasks: []
-                };
-            },
-        }
+        projectList.refreshProjects();
+        return projectList;
     })
 
-    .controller('TaskListCtrl', function ($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
+    /**
+     * Controller for the displayed task list
+     */
+    .controller('TaskListCtrl', function ($scope, ProjectList, $ionicSideMenuDelegate) {
 
-        // A utility function for creating a new project
-        // with the given projectTitle
-        var createProject = function (projectTitle) {
-            var newProject = Projects.newProject(projectTitle);
-            $scope.projects.push(newProject);
-            Projects.updateServerData($scope.projects);
-            $scope.selectProject(newProject, $scope.projects.length - 1);
-        }
+        $scope.getProjects = ProjectList.getProjects;
+        $scope.getActiveProject = ProjectList.getActiveProject;
 
-
-        var checkShouldCreateInitialProject = function () {
-            if ($scope.projects.length == 0) {
-                while (true) {
-                    var projectTitle = prompt('Your first project title:');
-                    if (projectTitle) {
-                        createProject(projectTitle);
-                        break;
-                    }
-                }
-            }
-        };
-
-        var loadProjectsFromServer = function (additionalSuccess) {
-            // Load or initialize projects
-            Projects.loadFromServer().success(
-                function (value) {
-                    console.log("**" + value);
-                    $scope.projects = angular.fromJson(value);
-                    $scope.activeProject = $scope.projects[0];
-                    console.log($scope.projects);
-                    additionalSuccess();
-                }).error(function (error) {
-                alert(error);
-            });
-        };
-
-        loadProjectsFromServer(checkShouldCreateInitialProject);
-
-        // Called to create a new project
-        $scope.newProject = function () {
-            var projectTitle = prompt('Project name');
-            if (projectTitle) {
-                createProject(projectTitle);
-            }
-        };
-
-        // Called to select the given project
-        $scope.selectProject = function (project, index) {
-            $scope.activeProject = project;
-            Projects.setLastActiveIndex(index);
-            $ionicSideMenuDelegate.toggleRight(false);
-        };
-
-        // Create our modal
-        $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
-            $scope.taskModal = modal;
-        }, {
-            scope: $scope
-        });
-
-        $scope.updateServerData = function () {
-            Projects.updateServerData($scope.projects);
-        };
-
-        $scope.createTask = function (task) {
-            if (!$scope.activeProject || !task) {
-                return;
-            }
-            $scope.activeProject.tasks.push({
-                title: task.title,
-                active: false
-            });
-            $scope.taskModal.hide();
-
-            $scope.updateServerData();
-
-            task.title = "";
-        };
-
-        $scope.newTask = function () {
-            $scope.taskModal.show();
-        };
-
-        $scope.closeNewTask = function () {
-            $scope.taskModal.hide();
-        }
+        $scope.onChangeTaskActive = ProjectList.updateProjects;
 
         $scope.toggleProjects = function () {
             $ionicSideMenuDelegate.toggleRight();
         };
 
-
-        $scope.completeTask = function (task) {
-            var index = $scope.activeProject.tasks.indexOf(task);
-            $scope.activeProject.tasks.splice(index, 1);
-            Projects.updateServerData($scope.projects);
-        };
-
         $scope.refreshTaskList = function () {
-            loadProjectsFromServer(function () {
+            ProjectList.refreshProjects(function() {
                 $scope.$broadcast('scroll.refreshComplete');
             });
         };
+
+        $scope.onSwipeRightTaskItem = function(task) {
+            ProjectList.removeTask(task);
+        };
+
+        $scope.range = function(num){
+            var x=new Array(); for(var i=0;i<num;i++){ x.push(i+1); } return x;
+        };
+    })
+
+    /**
+     * Controller for the project selection and creation panel
+     */
+    .controller('SelectProjectCtrl', function($scope, $ionicSideMenuDelegate, ProjectList){
+        // Called to create a new project
+        $scope.newProject = function () {
+            var projectTitle = prompt('Project name');
+            if (projectTitle) {
+                ProjectList.addNewProject(projectTitle);
+                $ionicSideMenuDelegate.toggleRight(false);
+            }
+        };
+
+        $scope.selectProject = function(project){
+            ProjectList.setActiveProject(project);
+            $ionicSideMenuDelegate.toggleRight(false);
+        };
+
+        $scope.onSwipeRightProjectItem = function(project){
+            console.log("Swiped!!");
+            ProjectList.removeProject(project);
+        }
+    })
+
+    /**
+     * Controller for the task creation modal
+     */
+    .controller('NewTaskModalCtrl', function(ProjectList, $scope, $ionicModal){
+        $scope.createTaskModalFunctions = {
+            initialize: function() {
+                $scope.newTask = {title: "", importance: 3, time: 3};
+
+                // Create our modal
+                $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+                    $scope.taskModal = modal;
+                }, {
+                    scope: $scope
+                });
+            },
+
+            submit: function () {
+                ProjectList.getActiveProject().addNewTask($scope.newTask);
+                ProjectList.updateProjects();
+
+                $scope.taskModal.hide();
+                $scope.newTask.title = "";
+                $scope.newTask.importance = 3;
+                $scope.newTask.time = 3;
+            },
+
+            openModal: function () {
+                $scope.taskModal.show();
+            },
+
+            hideModal: function () {
+                $scope.taskModal.hide();
+            }
+        };
+        $scope.createTaskModalFunctions.initialize();
     });
 
