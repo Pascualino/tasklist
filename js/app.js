36b69f7221964adf9e872937249956e411dab6dc
Angel Pina <pascualinogm@gmail.com>
2016-01-31 14:34:38 +0100

    Removing 'projects', limiting only to a single task list. Simplifying project complexity

--- js/app.js
+++ js/app.js
@@ -45,65 +45,25 @@ angular.module('tasklist', ['ionic'])
     })
 
     /**
-     * Creates Project objects
+     * Keeps and synchronizes the list of tasks
      */
-    .factory('ProjectFactory', ['TaskFactory', function(TaskFactory){
-        return {
-            newProject: function(projectTitle, tasks){
-                return {
-                    title: projectTitle,
-                    tasks: tasks,
-                    addNewTask: function(task){
-                        this.tasks.push(TaskFactory.newEmptyTask(task));
-                    },
-                    removeTask: function(task){
-                        var index = this.tasks.indexOf(task);
-                        this.tasks.splice(index, 1);
-                    }
-                };
-            },
-            newEmptyProject: function(projectTitle) {
-                return this.newProject(projectTitle, []);
-            },
-            projectFromJson: function(project){
-                var tasks = [];
-                project.tasks.forEach(function(element){
-                    tasks.push(TaskFactory.taskFromJson(element));
-                });
+    .factory('TaskList', function(PersistenceService, TaskFactory, $ionicLoading){
+        var tasks = [];
 
-                return this.newProject(project.title, tasks);
-            }
-        }
-    }])
-
-    /**
-     * Keeps and synchronizes the list of projects with their tasks, as well as the currently active project
-     */
-    .factory('ProjectList', function(PersistenceService, ProjectFactory, $ionicLoading){
-        var projects = [];
-        var activeProject = {};
-
-        var projectList = {
-            getProjects: function(){
-                return projects;
+        var taskList = {
+            getTasks: function(){
+                return tasks;
             },
-            getActiveProject: function(){
-                return activeProject;
-            },
-            setActiveProject: function(project){
-                activeProject = project;
-            },
-            setProjectsFromJson: function(jsonProjects){
-                projects = [];
-                jsonProjects.forEach(function(e){
-                    projects.push(ProjectFactory.projectFromJson(e));
+            setTasksFromJson: function(jsonTasks){
+                tasks = [];
+                jsonTasks.forEach(function(e){
+                    tasks.push(TaskFactory.taskFromJson(e));
                 });
             },
-            refreshProjects: function (additionalActionsOnSuccess) {
+            refreshTasks: function (additionalActionsOnSuccess) {
                 PersistenceService.loadFromServer().success(
                     function (value) {
-                        projectList.setProjectsFromJson(angular.fromJson(value));
-                        activeProject = projects[0];
+                        taskList.setTasksFromJson(angular.fromJson(value));
                         console.log(value);
                         if(additionalActionsOnSuccess){
                             additionalActionsOnSuccess();
@@ -112,55 +72,43 @@ angular.module('tasklist', ['ionic'])
                     alert(error);
                 })
             },
-            updateProjects: function () {
-                PersistenceService.updateServerData(angular.toJson(projects))
+            updateTasks: function () {
+                PersistenceService.updateServerData(angular.toJson(tasks))
                     .success(function(){
                         $ionicLoading.show({ template: 'Content synchronized!', noBackdrop: true, duration: 2000 });
                     });
             },
-            addNewProject: function (projectTitle) {
-                var newProject = ProjectFactory.newEmptyProject(projectTitle);
-                projects.push(newProject);
-                activeProject = newProject;
-                projectList.updateProjects();
-            },
             removeTask: function(task){
-                activeProject.removeTask(task);
-                projectList.updateProjects();
+                var index = tasks.indexOf(task);
+                tasks.splice(index, 1);
+                taskList.updateTasks();
             },
-            removeProject: function(project){
-                var index = projects.indexOf(project);
-                projects.splice(index, 1);
-                console.log(angular.toJson(projects));
-                projectList.updateProjects();
-            }
+            addNewTask: function(task){
+                tasks.push(TaskFactory.newEmptyTask(task));
+                taskList.updateTasks();
+            },
         };
-        projectList.refreshProjects();
-        return projectList;
+        taskList.refreshTasks();
+        return taskList;
     })
 
     /**
      * Controller for the displayed task list
      */
-    .controller('TaskListCtrl', function ($scope, ProjectList, $ionicSideMenuDelegate) {
+    .controller('TaskListCtrl', function ($scope, TaskList, $ionicSideMenuDelegate) {
 
-        $scope.getProjects = ProjectList.getProjects;
-        $scope.getActiveProject = ProjectList.getActiveProject;
+        $scope.getTasks = TaskList.getTasks;
 
-        $scope.onChangeTaskActive = ProjectList.updateProjects;
-
-        $scope.toggleProjects = function () {
-            $ionicSideMenuDelegate.toggleRight();
-        };
+        $scope.onChangeTaskActive = TaskList.updateTasks;
 
         $scope.refreshTaskList = function () {
-            ProjectList.refreshProjects(function() {
+            TaskList.refreshTasks(function() {
                 $scope.$broadcast('scroll.refreshComplete');
             });
         };
 
         $scope.onSwipeRightTaskItem = function(task) {
-            ProjectList.removeTask(task);
+            TaskList.removeTask(task);
         };
 
         $scope.range = function(num){
@@ -169,33 +117,9 @@ angular.module('tasklist', ['ionic'])
     })
 
     /**
-     * Controller for the project selection and creation panel
-     */
-    .controller('SelectProjectCtrl', function($scope, $ionicSideMenuDelegate, ProjectList){
-        // Called to create a new project
-        $scope.newProject = function () {
-            var projectTitle = prompt('Project name');
-            if (projectTitle) {
-                ProjectList.addNewProject(projectTitle);
-                $ionicSideMenuDelegate.toggleRight(false);
-            }
-        };
-
-        $scope.selectProject = function(project){
-            ProjectList.setActiveProject(project);
-            $ionicSideMenuDelegate.toggleRight(false);
-        };
-
-        $scope.onSwipeRightProjectItem = function(project){
-            console.log("Swiped!!");
-            ProjectList.removeProject(project);
-        }
-    })
-
-    /**
      * Controller for the task creation modal
      */
-    .controller('NewTaskModalCtrl', function(ProjectList, $scope, $ionicModal){
+    .controller('NewTaskModalCtrl', function(TaskList, $scope, $ionicModal){
         $scope.createTaskModalFunctions = {
             initialize: function() {
                 $scope.newTask = {title: "", importance: 3, time: 3};
@@ -209,8 +133,7 @@ angular.module('tasklist', ['ionic'])
             },
 
             submit: function () {
-                ProjectList.getActiveProject().addNewTask($scope.newTask);
-                ProjectList.updateProjects();
+                TaskList.addNewTask($scope.newTask);
 
                 $scope.taskModal.hide();
                 $scope.newTask.title = "";
